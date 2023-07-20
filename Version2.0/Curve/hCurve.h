0b899b2ee507aeb91a03b58532f22f2e978f42cd
aevarel <113391075+aevarel@users.noreply.github.com>
2023-07-20 11:08:44 -0700

    Updated hCurve
    Updated hCurve to encompass implementation

--- Version2.0/Curve/hCurve.h
+++ Version2.0/Curve/hCurve.h
@@ -2,57 +2,43 @@
 #define CSC212_PROJECT_2_HCURVE_H
 #include <vector>
 #include <SFML/Graphics.hpp>
-#include <valarray>
-#include <cmath>
-////////////////////////////////////////////////////////////////////////////
-class Point{
-////////////////////////////////////////////////////////////////////////////
-private:
-    int x;
-    int y;
 
-public:
-    // Default constructor // Parameters are necessary.
-    Point(double x, double y);
-};
-////////////////////////////////////////////////////////////////////////////////////////////////
 class hCurve{
-/////////////////////////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////
-    private:
-    ////////// Private Member Variables //////////////////////////////////
+private:
+    ////////// private member variables //////////////////////////////////
+    float topright;
+    float topleft;
+    float bottomleft;
+    float bottomright;
     // Order of the curve generation, represents how many times we will be recursively generating/calling the algorithm from points.
     int order;
     // The initial size of the grid, used to represent the grid size by (size x size) length x height.
-    int size;
-    // Starting x-value.
-    double startX;
-    // Starting y-value.
-    double startY;
-    // Vector used to hold the points of the curve prior to generating graphics.
-    std::vector<Point> *points;
-    ///////////////////////////////////////////////////////////////////
-    public:
-    ////////// Public Member Functions  //////////////////////////////////
+    float size;
+    sf::Color colorGraphic;
+public:
+    sf::RectangleShape invisible_box;
     // Default constructor // Parameters are necessary.
-    hCurve(int order, double size, double x, double y);
+    hCurve();
     // Deconstructor.
     ~hCurve();
 
     // Getter Functions.
     int getOrder();
-    int getSize();
-    int getStart(double x, double y);
+    float getSize();
+
     // Setter Functions.
     void setOrder(int order);
-    double setSize(double size);
-    void setStart(double x, double y);
+    void setSize(float size);
+    // generates and returns the curve
+    sf::Vector2f generateCurve(int order, float size, sf::Vector2f currentCoord, int travrseDir);
+    void drawGraphic(sf::RenderWindow& graphic_window);
+    std::vector<sf::Vector2f> coordinates;
 
-    // Generates and returns the curve through a series of recursive calls into a vector of Point objects.
-    void generateCurve(int order, double size, double x, double y);
+//    void setColor(const sf::Color& color);
+    int drawCup(sf::Vector2f currentPos, int direction);
 
-    // Friend class Point
-    friend class Point;
+    sf::Vector2f drawHilbertCurve(int order, int direction, sf::Vector2f currentCoord);
 };
+
 
 #endif //CSC212_PROJECT_2_HCURVE_H
